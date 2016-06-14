56bdd3e5175671e10909a31750e880b9e3354125
Jeff Hutchings <hutchings90@gmail.com>
2016-06-14 10:44:58 -0600

    Added bouncing, Fruits launch from back right towards left

--- js/main.js
+++ js/main.js
@@ -7,20 +7,31 @@ document.body.appendChild(renderer.domElement);
 
 var fruits = [];
 
-function gravitate(fruit){
-	fruit.velocity.y -= GRAVITY;
+function gravitate(fruit) {
+    fruit.velocity.y -= GRAVITY;
 }
 
 function render() {
+    var removeFruits = [];
     requestAnimationFrame(render);
 
     for (fruit of fruits) {
-		fruit.position.x += fruit.velocity.x;
-		fruit.position.y += fruit.velocity.y;
-		fruit.position.z -= fruit.velocity.z;
-		gravitate(fruit);
+        fruit.position.x += fruit.velocity.x;
+        if (fruit.position.y < 0) {
+            fruit.velocity.y *= -1;
+        }
+        fruit.position.y += fruit.velocity.y;
+        fruit.position.z -= fruit.velocity.z;
+        gravitate(fruit);
         fruit.rotation.x += .1;
         fruit.rotation.y += .1;
+        if (fruit.position.z < -1000) {
+            removeFruits.push(fruit);
+        }
+    }
+    for (fruit of removeFruits) {
+        fruits.splice(fruits.indexOf(fruit), 1);
+        scene.remove(fruit);
     }
 
     renderer.render(scene, camera);
@@ -30,15 +41,13 @@ function fruit() {
     var geometry = new THREE.SphereGeometry(camera.fov * .1, 5, 1);
     var material = new THREE.MeshNormalMaterial({});
     var fruit = new THREE.Mesh(geometry, material);
-	fruit.position.x = Math.random() * (camera.fov / 2);
-	fruit.position.x *= Math.random() < .5 ?  -1 : 1;
-	fruit.position.y = -camera.fov / 2;
-	fruit.position.z = -Math.random() * camera.fov * .1;
-	fruit.velocity = {};
-	fruit.velocity.x = 0;
-	fruit.velocity.y = Math.random() * 10 + 10;
-	fruit.velocity.z = 5;
-	console.log(fruit);
+    fruit.position.x = Math.random() * (camera.fov / 4) + (camera.fov / 4);
+    fruit.position.y = 0;
+    fruit.position.z = -Math.random() * camera.fov * .1;
+    fruit.velocity = {};
+    fruit.velocity.x = 0;
+    fruit.velocity.y = Math.random() * 10 + 10;
+    fruit.velocity.z = 5;
     fruits.push(fruit);
     scene.add(fruit);
 };
